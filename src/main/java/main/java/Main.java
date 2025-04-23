1a5ede9d35b51b4d31f43856b50b7644bbe9fbfb
mijinn0 <pmj_0818@naver.com>
2025-04-23 14:13:52 +0900

    20250423 JDBC CRUD(DAO) MVC 패턴

--- src/main/java/main/java/Main.java
+++ src/main/java/main/java/Main.java
@@ -1,15 +1,40 @@
 package main.java;
 
-import main.java.manager.MemberManager;
+/*
+* 1. 회원 CRUD 프로젝트 MemberManager
+*   - dao: MemberDao
+*   - db: DBConnection
+*   - entity: Member
+*   - manage: MemberManager
+*
+* 2. 게시판 CRUD 프로젝트 (+ MVC 패턴)
+*   - dao: PostDao
+*   - db: DBConnection
+*   - entity: Post
+*   - service (비즈니스 로직 처리): PostService
+*   - view: PostView
+*   - manager: PostManager
+* */
+
+import main.java.manager.PostManager;
 
 public class Main {
     public static void main(String[] args) {
-        MemberManager mm = new MemberManager();
+//        MemberManager mm = new MemberManager();
+//
+//        try {
+//            mm.run();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        PostManager pm = new PostManager();
 
         try {
-            mm.run();
+            pm.run();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 }
